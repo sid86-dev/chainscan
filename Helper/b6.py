97c6ff15092cfe52aa9961e78722bb60db542bbb
sid86-dev <sid86harth@gmail.com>
2022-02-06 02:21:59 +0530

    web3

--- Helper/b6.py
+++ Helper/b6.py
@@ -8,5 +8,7 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 # data = web3.eth.get_transaction('0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060')
 data = web3.eth.get_block('latest')
 
-for i in data:
-	print(i)
+if __name__ == "__main__":
+
+	for i in data:
+		print(i)
--- github.sh
+++ github.sh
@@ -1,5 +1,5 @@
 clear
-git add.;
+git add .;
 echo "Enter your commit: "
 read commit
 git commit -m $commit;
